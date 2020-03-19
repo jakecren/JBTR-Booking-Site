3543f50228f40e449f130059552c8a0ce34adb14
jakecren <51045803+jakecren@users.noreply.github.com>
2020-03-19 21:16:09 +1000

    rsvp Form Inital Functionaity

--- bookingApp/main/routes.py
+++ bookingApp/main/routes.py
@@ -1,4 +1,7 @@
 from flask import render_template, request, Blueprint, redirect, url_for, flash
+from bookingApp.main.forms import rsvpForm
+from bookingApp import db
+from bookingApp.models import Customers
 
 
 main = Blueprint("main", __name__)
@@ -7,6 +10,16 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def splash():
     return render_template("splash.html", title="JBTR RSVP")
+
+
+#####  RSVP  #####
+@main.route("/rsvp", methods=["GET", "POST"])
+def rsvp():
+    form = rsvpForm()
+    if form.validate_on_submit():
+        flash(f"{form.forename.data}, {form.surname.data}, {form.email.data}, {form.mobile.data}, {form.street.data}, {form.suburb.data}, {form.city.data}, {form.state.data}, {form.postcode.data}.")
+
+    return render_template("rsvp.html", title="RSVP", form=form)
 
 
 #####  Generic  #####
