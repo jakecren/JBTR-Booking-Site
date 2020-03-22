4bf4cdcb6bb9681f59cd4455a34c3c839c5a29df
jakecren <51045803+jakecren@users.noreply.github.com>
2020-03-22 18:17:56 +1000

    Register Vendor Basic Functionality

--- bookingApp/users/forms.py
+++ bookingApp/users/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
-from bookingApp.models import Users
+from bookingApp.models import Users, Vendors
 
 
 class LoginForm(FlaskForm):
@@ -10,3 +10,46 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password:", validators=[DataRequired()])
     remember = BooleanField("Remember Me:")
     submit = SubmitField("Log In")
+
+
+class RegisterVendorForm(FlaskForm):
+    # Account Info
+    forename = StringField("Forename:", validators=[DataRequired()])
+    surname = StringField("Surname:", validators=[DataRequired()])
+    email = StringField("Email:", validators=[DataRequired(), Email()])
+    mobile = StringField("Mobile:", validators=[DataRequired(), Length(10,12)])
+    password = PasswordField("Password:", validators=[DataRequired()])
+    confirmPassword = PasswordField("Confirm Password:", validators=[DataRequired(), EqualTo("password")])
+
+    # Company Info
+    companyName = StringField("Company Name:", validators=[DataRequired()])
+    companyEmail = StringField("Company Email:", validators=[DataRequired(), Email()])
+    companyMobile = StringField("Company Mobile:", validators=[DataRequired(), Length(10,12)])
+
+    submit = SubmitField("Register")
+
+    # Validate unique fields
+    def validate_mobile(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email is unavailable, please choose a different email.")
+
+    def validate_mobile(self, mobile):
+        user = Users.query.filter_by(mobile=mobile.data).first()
+        if user:
+            raise ValidationError("Mobile is unavailable, please choose a different mobile.")
+
+    def validate_companyName(self, companyName):
+        vendor = Vendors.query.filter_by(name=companyName.data).first()
+        if vendor:
+            raise ValidationError("Company name is unavailable, please choose a different Company name.")
+    
+    def validate_companyEmail(self, companyEmail):
+        vendor = Vendors.query.filter_by(email=companyEmail.data).first()
+        if vendor:
+            raise ValidationError("Company email is unavailable, please choose a different Company email.")
+    
+    def validate_companyMobile(self, companyMobile):
+        vendor = Vendors.query.filter_by(mobile=companyMobile.data).first()
+        if vendor:
+            raise ValidationError("Company mobile is unavailable, please choose a different Company mobile.")
--- bookingApp/users/routes.py
+++ bookingApp/users/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, url_for, flash, redirect, request, Blueprint, current_app
+from flask import render_template, url_for, flash, redirect, request, Blueprint, current_app, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from bookingApp import db, bcrypt
-from bookingApp.users.forms import LoginForm
+from bookingApp.users.forms import LoginForm, RegisterVendorForm
 from bookingApp.models import Users, Vendors
 
 users = Blueprint("users", __name__)
@@ -37,3 +37,28 @@ def logout():
 def admin():
     vendors = Vendors.query.all()
     return render_template("admin.html", title="Admin", vendors=vendors)
+
+
+##### Register Vendor #####
+@users.route("/registerVendor", methods=["GET", "POST"])
+@login_required
+def registerVendor():
+    if current_user.admin != 1:
+        abort(403)
+    form = RegisterVendorForm()
+    if form.validate_on_submit():
+        hashedPassword = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        Email = str(form.email.data).lower()
+
+        user = Users(forename=form.forename.data, surname=form.surname.data, email=Email, mobile=form.mobile.data, password=hashedPassword, admin=0)
+        db.session.add(user)
+        db.session.commit()
+
+        user = Users.query.filter_by(email=Email).first_or_404()
+        vendor = Vendors(name=form.companyName.data, email=form.companyEmail.data, mobile=form.companyMobile.data, userID=user.id)
+        db.session.add(vendor)
+        db.session.commit()
+
+        flash("Vendor Registered", "success")
+        return redirect(url_for("users.admin"))
+    return render_template("registerVendor.html", title="Register Vendor", form=form)
