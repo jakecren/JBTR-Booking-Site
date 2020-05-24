3df502e7fd1ad484bfb42a3647aac5e1f5ada60f
Jake Rennie <51045803+jakecren@users.noreply.github.com>
2020-05-24 15:32:21 +1000

    Last Bug Fixes

--- bookingApp/main/routes.py
+++ bookingApp/main/routes.py
@@ -30,7 +30,6 @@ def rsvp():
         customer = [form.forename.data, form.surname.data, Email, form.mobile.data, form.street.data, form.suburb.data, form.city.data, form.state.data, form.postcode.data]
         session['customer'] = customer
         session.modified = True
-        flash("Hell Yeah", "success")
         
         return redirect(url_for('main.rsvp_2'))
     return render_template("main/rsvp_1.html", title="RSVP", form=form, customer=customer)
@@ -67,7 +66,6 @@ def rsvp_2():
     
     if session.get('orderedProducts') != None:
         return render_template("main/rsvp_2.html", title="RSVP", form=form, products=products, customerOrders=customerOrders)
-        print("Customer Orders!")
     else:
         return render_template("main/rsvp_2.html", title="RSVP", form=form, products=products)
 
@@ -80,7 +78,6 @@ def rsvp_3():
 
     products = Products.query.all()
     form = rsvpForm_3()
-    orders = Orders.query.all()
     customer = session.get('customer')
 
     customerOrders = []
@@ -106,18 +103,18 @@ db.session.add(order)""")
 
         message = Mail(
             from_email='donotreply@atcjbtrrsvp.com',
-            to_emails="jakecrennie@gmail.com",
-            subject='RSVP Successful',
-            html_content='Hell yeah boi!')
+            to_emails=customer[2],
+            subject='RSVP Successful')
         message.template_id = "d-25f51cfbf3ca459585aa2a61ff4feb82"
         with open('bookingApp/main/templates/main/emails/receipt.html', 'r') as f:
             content = f.read()
     
+        orders = Orders.query.all()
         total = 0.00
         contentStr = ""
         for order in orders:
             for product in products:
-                if order.referenceNumber == 10:
+                if order.referenceNumber == Customer.id:
                     if product.id == order.productID:
                         total += (order.quantity * product.price)
                         if product.name[:2] == "t_":
@@ -129,58 +126,20 @@ db.session.add(order)""")
                             <td>{productName.replace("_", " ")}</td>
                             <td>{order.quantity}</td>
                             <td>${product.price}</td>
-                            <td>${(product.price * order.quantity)}</td>
+                            <td>${round((product.price * order.quantity), 2)}</td>
                         </tr>
                         """
 
         message.dynamic_template_data = {
-            'content': content.format(tbody=contentStr, total=total)}
-
+            'content': content.format(tbody=contentStr, total=round(total, 2)),
+            'name': (customer[0] + " " + customer[1])
+            }
         SGmail.send(message)
 
+        flash("RSVP Successful!  Please check your email for a copy of the order.")
         return redirect(url_for('main.splash'))
     return render_template("main/rsvp_3.html", title="RSVP", form=form, products=products, customer=customer, customerOrders=customerOrders)
 
-@main.route("/email")
-def email():
-    products = Products.query.all()
-    orders = Orders.query.all()
-
-    message = Mail(
-        from_email='donotreply@atcjbtrrsvp.com',
-        to_emails="jakecrennie@gmail.com",
-        subject='RSVP Successful',
-        html_content='Hell yeah boi!')
-    message.template_id = "d-25f51cfbf3ca459585aa2a61ff4feb82"
-    with open('bookingApp/main/templates/main/emails/test.html', 'r') as f:
-        content = f.read()
-    
-    total = 0.00
-    contentStr = ""
-    for order in orders:
-        for product in products:
-            if order.referenceNumber == 10:
-                if product.id == order.productID:
-                    total += (order.quantity * product.price)
-                    if product.name[:2] == "t_":
-                        productName = product.name[2:]
-                    else:
-                        productName = product.name
-                    contentStr += f"""
-                    <tr>
-                        <td>{productName.replace("_", " ")}</td>
-                        <td>{order.quantity}</td>
-                        <td>${product.price}</td>
-                        <td>${(product.price * order.quantity)}</td>
-                    </tr>
-                    """
-
-    message.dynamic_template_data = {
-        'content': content.format(tbody=contentStr, total=total)}
-
-    SGmail.send(message)
-
-    return redirect(url_for('main.splash'))
 
 #####  Login  #####
 @main.route("/login", methods=["GET", "POST"])
