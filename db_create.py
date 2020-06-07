4d853a6254191eab0e959a81e2fe2fc406ef6c93
Jake Rennie <51045803+jakecren@users.noreply.github.com>
2020-06-07 23:05:02 +1000

    db_create default admin info updated

--- db_create.py
+++ db_create.py
@@ -2,5 +2,5 @@ from bookingApp import db, create_app
 from bookingApp.models import *
 import bcrypt
 db.create_all(app=create_app())
-db.session.add(lambda: Users(forename="admin", email="change@me.please", password=bcrypt.generate_password_hash("password").decode("utf-8")))
+db.session.add(lambda: Users(forename="admin", email="admin@atc.jbtr", password=bcrypt.generate_password_hash("admin").decode("utf-8"), admin=1))
 db.session.commit()
